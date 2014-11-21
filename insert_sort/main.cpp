0b4ccc9728197717de02c78a56e1ff1ab3971bd5
wuqiankun <qiankun@nfschina.com>
2014-11-21 10:46:53 +0800

    insert_sort,升序和降序的实现

--- insert_sort/main.cpp
+++ insert_sort/main.cpp
@@ -20,7 +20,9 @@ int main()
     int array[]={1,100,33,21,88,20,67,33,87,48};
    size_t num= sizeof(array)/sizeof(int);
    cout << num<<"\n";
-    for(int j=1;j<num;++j)
+ /*
+   //test 升序
+   for(int j=1;j<num;++j)
     {
         int var=array[j];
         cout<<var<<"\n";
@@ -32,6 +34,20 @@ int main()
         }
         array[i+1]=var;
     }
+    display(array,num);
+  */
+   //test2 降序
+   for(int m=1;m<num;++m)
+   {
+       int var=array[m];
+       int n=m-1;
+       while(n>=0 && var > array[n])
+       {
+           array[n+1]=array[n];
+           --n;
+       }
+       array[n+1]=var;
+   }
     display(array,num);
 
     cout << "Hello World!" << endl;
