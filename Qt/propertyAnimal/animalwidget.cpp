663595ba2b0f0794792d4a2100a2d812d35b8be1
wuqiankun <qiankun@nfschina.com>
2015-01-13 10:47:16 +0800

    动画属性QT

---
+++ Qt/propertyAnimal/animalwidget.cpp
@@ -0,0 +1,25 @@
+#include "animalwidget.h"
+
+#include <QPushButton>
+#include <QHBoxLayout>
+
+AnimalWidget::AnimalWidget(QWidget *parent) :
+    QWidget(parent)
+{
+   QPushButton * m_widgte=new QPushButton;
+   QHBoxLayout * H=new QHBoxLayout;
+   H->addWidget(m_widgte);
+   //m_widgte->setBaseSize(200,200);
+   m_animal=new QPropertyAnimation(m_widgte,"geometry");
+   m_animal->setDuration(300000);//sudu
+   m_animal->setStartValue(QRect(0, 0, 100, 30));
+   m_animal->setEndValue(QRect(250, 250, 100, 300));
+   connect(m_widgte,SIGNAL(clicked()),this,SLOT(start()));
+   this->setLayout(H);
+
+}
+void AnimalWidget::start()
+{
+
+  m_animal->start();
+}
---
+++ Qt/propertyAnimal/propertyAnimal.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-01-13T10:09:47
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       += gui
+
+TARGET = propertyAnimal
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    animalwidget.cpp
+
+HEADERS += \
+    animalwidget.h
