afd543bc2a736e1b90c34480d7d11c0002d1b7d7
wuqiankun <qiankun@nfschina.com>
2015-12-23 09:34:14 +0800

    testTranslate

---
+++ qtProjectTest/testTranslate/main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QFileDialog>
+#include <QObject>
+#include <QTranslator>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+  QTranslator _t;
+  _t.load("/home/qiankun.qm");
+  qApp->installTranslator(&_t);
+
+  QString  fileName = QFileDialog::getSaveFileName(NULL, \
+        QObject::tr("Open Image"), "/home/",  QObject::tr("Image Files (*.png *.jpg *.bmp)"));
+    return a.exec();
+}
---
+++ qtProjectTest/testTranslate/testTranslate.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-12-23T09:21:28
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = testTranslate
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES += \
+    res.qrc
